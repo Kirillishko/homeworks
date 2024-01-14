266f1b23f9253775223cd2925540caa8823fd903
Kirillishko <97043155+Kirillishko@users.noreply.github.com>
2024-01-14 13:27:47 +0500

    fix: заменил for of на reduce

--- src/js/MainPageRenderer.js
+++ src/js/MainPageRenderer.js
@@ -19,31 +19,33 @@ searchInput.addEventListener('input', (e) => {
 });
 
 const renderProducts = (products) => {
-	let html = '';
-	for (const product of products) {
-		const { id, like, picture, name, price } = product;
+	const html = products.reduce((accumulator, value) => {
+		const { id, like, picture, name, price } = value;
 		const priceWithSign = convertCurrencyToSign(price.currency) + price.value;
 		const imagePath = getImagePath(picture.path);
 
-		html += `   <figure class="card">
-		                <label class="favorite">
-			                <input type="checkbox" ${like && 'checked'}/>
-			                <svg class="favorite-icon">
-				               <use href="#favorite-icon"></use>
-			                </svg>
-			                <svg class="favorite-active-icon">
-			                	<use href="#favorite-active-icon"></use>
-			                </svg>
-		                </label>
-		                <a href="product.html?id=${id}"><img alt="${picture.alt}" src="${imagePath}" /></a>
-		            	<figcaption class="info">
-		                    <h2>
-		                        <a href="product.html?id=${id}">${name}</a>
-		                    </h2>
-		                    <p>${priceWithSign}</p>
-		                </figcaption>
-		            </figure>`;
-	}
+		return (
+			accumulator +
+			`   <figure class="card">
+						<label class="favorite">
+							<input type="checkbox" ${like && 'checked'}/>
+							<svg class="favorite-icon">
+								<use href="#favorite-icon"></use>
+							</svg>
+							<svg class="favorite-active-icon">
+								<use href="#favorite-active-icon"></use>
+							</svg>
+						</label>
+						<a href="product.html?id=${id}"><img alt="${picture.alt}" src="${imagePath}" /></a>
+						<figcaption class="info">
+							<h2>
+								<a href="product.html?id=${id}">${name}</a>
+							</h2>
+							<p>${priceWithSign}</p>
+						</figcaption>
+					</figure>`
+		);
+	}, '');
 
 	main.innerHTML = html;
 };
